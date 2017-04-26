f7431269e8a241c6222766b06af1de3a53d7df17
annie ke <annieke8@gmail.com>
2017-04-26 11:30:36 -0400

    added fallback

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 import './style.scss';
 
 const Nav = (props) => {
   return (
     <nav>
       <ul>
-        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/" exact>Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
       </ul>
     </nav>
   );
@@ -19,14 +21,24 @@ const About = (props) => {
 const Welcome = (props) => {
   return <div>Welcome</div>;
 };
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
 
 const App = () => {
   return (
     <Router>
       <div>
         <Nav />
-        <Route exact path="/" component={Welcome} />
-        <Route path="/about" component={About} />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
       </div>
     </Router>
   );
