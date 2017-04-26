78508c7dd86181ce143f399a41240ddeeccdbf52
annie ke <annieke8@gmail.com>
2017-04-26 11:14:53 -0400

    added react router, new index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import './style.scss';
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
 
 const App = () => {
-  return <div className="test">All the React are belong to us!</div>;
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Route exact path="/" component={Welcome} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('main'));
