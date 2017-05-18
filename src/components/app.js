d8d17b8339186e79fdc7b72ac56e96361222b7d9
SiaSyPeng <shiyao.peng.20@dartmouth.edu>
2017-05-17 20:00:53 -0400

    profile component structure

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import Profile from './containers/profile';
 import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 
 const Nav = (props) => {
