4028d07b9c633817f97a06a48300248bb2a990b9
Eun Kyung Yoon <ekyoon@hyuns-mbp.wireless.dartmouth.edu>
2017-05-18 20:47:32 -0400

    signinup routing

--- src/containers/signin.js
+++ src/containers/signin.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter, NavLink } from 'react-router-dom';
+import { withRouter, NavLink, Link } from 'react-router-dom';
 import { signinUser } from '../actions';
-
 
 class SignIn extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
       username: '',
       password: '',
     };
@@ -18,7 +16,6 @@ class SignIn extends Component {
     this.onPasswordChange = this.onPasswordChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
-
 
   onUsernameChange(event) {
     this.setState({ username: event.target.value });
@@ -33,22 +30,26 @@ class SignIn extends Component {
     this.props.signinUser(this.state, this.props.history);
   }
 
-
   render() {
     return (
       <div className="container">
         <div className="signin-form">
+          <span> Sign In </span>
           <div>
             <input placeholder="username" onChange={this.onUsernameChange} value={this.state.username} />
           </div>
           <div>
             <input placeholder="password" onChange={this.onPasswordChange} value={this.state.password} />
           </div>
-
         </div>
         <div id="buttons">
           <button onClick={this.onSubmit}>Submit</button>
           <NavLink to="/"><button>Cancel</button></NavLink>
+        </div>
+        <div>
+          <span>Wanna </span>
+          <Link to="/signup">Sign Up</Link>
+          <span>?</span>
         </div>
       </div>
 
