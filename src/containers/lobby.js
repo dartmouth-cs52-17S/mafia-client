9ebed7777f1f7bbca8975931317892c18ecec12b
SiaSyPeng <shiyao.peng.20@dartmouth.edu>
2017-05-23 16:17:15 -0400

    lobby structure in comment

--- src/containers/lobby.js
+++ src/containers/lobby.js
@@ -31,6 +31,38 @@ class Lobby extends Component {
     }, 2000);
   }
 
+  // renderPlayers() {
+  //   setTimeout(() => {
+  //     return this.props.players.map((player) => {
+  //       return (<li>{player}</li>);
+  //     });
+  //   }, 2000);
+  // }
+
+  // renderRoles() {
+  //   return (
+  //     <div className="RolesContainer">
+  //       <h2>You Are</h2>
+  //       {this.props.player.role}
+  //     </div>
+  //   );
+  // }
+
+  // renderChat() {
+  //
+  // }
+
+  // render() {
+  //   return (
+  //     <div className-"GameContainer">
+  //       <div className="Roles">{this.renderRoles}</div>
+  //       <div className="Roles">{}</div>
+  //       // <div className="Selection">{this.renderselection}</div>  // all the pop ups
+  //       // <div className="Chat">{this.renderChat}</div>
+  //     </div>
+  //   );
+  // }
+
   render() {
     return (
       <div>
@@ -46,6 +78,8 @@ class Lobby extends Component {
 const mapStateToProps = state => (
   {
     game: state.game,
+    players: state.players,
+    player: state.player,
   }
 );
 
