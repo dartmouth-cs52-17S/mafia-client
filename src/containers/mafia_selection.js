d2c9a3e8172270d37bdba3171bccacc0a3e92ca9
Eun Kyung Yoon <eun.kyung.yoon.19@dartmouth.edu>
2017-05-26 21:50:16 -0400

    stage 2,3,4

--- src/containers/mafia_selection.js
+++ src/containers/mafia_selection.js
@@ -1,53 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { killUser } from '../actions';
+import { killPlayer } from '../actions';
 
-class MafiaSel extends Component {
+class MafiaSelection extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      players: [],
-      selectedPlayer: null,
-    };
-    this.onKillClick = this.onKillClick.bind(this);
+    this.state = {};
+    this.onSelect = this.onSelect.bind(this);
   }
 
   onKillClick(event) {
-    this.props.killUser();
-    this.setState({ selectedPlayer: this.selectedPlayer });
+    this.props.killPlayer(event.target.key);
+  }
+
+  renderSelection() {
+    if (!localStorage.getItem('role')) { // this just checks if data has been fetched and mapped to props yet
+      return '';
+    } else if (localStorage.getItem('role') === 'mafia') {
+      return (
+       this.props.players.map((player) => {
+         return (
+           <div className="players_container">
+             <div className="playerName">{player.user.name}</div>
+             <button key={player._id} onClick={this.onKillClick}> {player.user} </button>
+           </div>
+         );
+       })
+      );
+    } else {
+      return <div className="wait">Waiting Mafia to Kill</div>;
+    }
   }
 
   render() {
-    // if (!this.props.players) { // this just checks if data has been fetched and mapped to props yet
-    //   return '';
-    // } else if (this.props.players.indexOf(this.props.user.id) === this.players[0]) { // if current player is mafia, show this
-    //   return (
-    //     <div className="players_container">
-    //       <ul>
-    //         <li onKillClick={this.onKillClicked}> {this.players[0].pic} </li>
-    //         <li onKillClick={this.onKillClicked}> {this.players[1].pic} </li>
-    //         <li onKillClick={this.onKillClicked}> {this.players[2].pic} </li>
-    //         <li onKillClick={this.onKillClicked}> {this.players[3].pic} </li>
-    //         <li onKillClick={this.onKillClicked}> {this.players[4].pic} </li>
-    //         <li onKillClick={this.onKillClicked}> {this.players[5].pic} </li>
-    //       </ul>
-    //     </div>
-    //   );
-    // } else { // if not mafia, show this
-    //   return <div />;
-    // }
-    return <div>stage1</div>;
+    return <div>{this.renderSelection()}</div>;
   }
+
 }
 
 
 const mapStateToProps = state => (
   {
     players: state.game.players,
-    user: state.users.user,
   }
 );
 
-export default withRouter(connect(mapStateToProps, { killUser })(MafiaSel));
+export default withRouter(connect(mapStateToProps, { killPlayer })(Selection));
