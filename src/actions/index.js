319cfa46e58d2d4efcaa0d6a33a9b4af33433159
arinehouse <arinehouse@yahoo.com>
2017-06-02 20:31:23 -0400

    more logs

--- src/actions/index.js
+++ src/actions/index.js
@@ -189,6 +189,7 @@ export function fetchGames() {
 export function fetchPlayers(gameID) {
   return (dispatch) => {
     axios.get(`${ROOT_URL}/players/${gameID}`).then((response) => {
+      console.log(response.data);
       // trim the payload to remove roles from the response
       const payload = response.data.map((fragment) => {
         return { id: fragment.id, userID: fragment.user, gameID: fragment.game, status: fragment.status, name: fragment.name };
